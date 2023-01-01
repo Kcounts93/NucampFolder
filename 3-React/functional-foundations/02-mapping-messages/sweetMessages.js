5fbe5f4205dd7269a686b457b8cf0dff4b304cfa
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-01-01 15:56:42 -0800

    candyhearts excercise

--- 3-React/functional-foundations/02-mapping-messages/sweetMessages.js
+++ 3-React/functional-foundations/02-mapping-messages/sweetMessages.js
@@ -4,8 +4,14 @@ const messages = [
     'would you like to get a bite later'
 ];
 
-// const sweetMessages = [
-//     'how are you doing, sweetie?',
-//     'what are you up to, sweetie?',
-//     'would you like to get a bite later, sweetie?',
-// ];
+// const sweetMessages = [];
+// for (let i = 0; i < messages.length; i++) {
+//     const newMessage = `${messages[i]}, sweetie?`;
+//     sweetMessages.push(newMessage);
+// 
+
+const sweetMessages = messages.map((message) => `${message}, sweetie?`);
+
+console.log(sweetMessages);
+
+
