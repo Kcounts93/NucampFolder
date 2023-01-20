6d87e8e781ef6bbc72fb456eb447f2ec416290a9
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-01-19 19:57:50 -0800

    ContactForm part3

--- 3-React/nucampsite/src/components/ContactForm.js
+++ 3-React/nucampsite/src/components/ContactForm.js
@@ -1,9 +1,12 @@
 import { Button, Label, Col, FormGroup } from "reactstrap";
 import { Formik, Field, Form } from "formik";
 
-import React from "react";
-
 export const ContactForm = () => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log("form values:", values);
+    console.log("in JSON format:", JSON.stringify(values));
+    resetForm();
+  };
   return (
     <Formik
       initialValues={{
@@ -14,6 +17,7 @@ export const ContactForm = () => {
         agree: false,
         contactType: "By Phone",
       }}
+      onSubmit={handleSubmit}
     >
       <Form>
         <FormGroup row>
@@ -90,7 +94,13 @@ export const ContactForm = () => {
             />
           </Col>
         </FormGroup>
-        <FormGroup row></FormGroup>
+        <FormGroup row>
+          <Col md={{ size: 10, offset: 2 }}>
+            <Button type="submit" color="primary">
+              Send Feedback
+            </Button>
+          </Col>
+        </FormGroup>
       </Form>
     </Formik>
   );
