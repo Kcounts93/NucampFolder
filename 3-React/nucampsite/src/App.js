e74dc3536f6c7a0a71785bde3b3f8128e61545a2
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-01-21 12:00:54 -0800

    component export

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,11 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
-import CampsiteDetailPage from "./pages/CampsiteDetailPage";
-import ContactPage from "./pages/ContactPage";
-import AboutPage from "./pages/AboutPage";
-import HomePage from "./pages/HomePage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+// import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+// import ContactPage from "./pages/ContactPage";
+// import AboutPage from "./pages/AboutPage";
+// import HomePage from "./pages/HomePage";
+import {
+  CampsitesDirectoryPage,
+  CampsiteDetailPage,
+  ContactPage,
+  AboutPage,
+  HomePage,
+} from "./pages";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+import { Header, Footer } from "./components";
 import "./App.css";
 
 function App() {
