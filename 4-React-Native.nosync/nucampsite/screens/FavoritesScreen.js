a45bc79a50594ee18bc8bcc6c6f197722823c516
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-03-01 17:27:34 -0800

    favorites

--- 4-React-Native.nosync/nucampsite/screens/FavoritesScreen.js
+++ 4-React-Native.nosync/nucampsite/screens/FavoritesScreen.js
@@ -9,22 +9,26 @@ const FavoritesScreen = ({ navigation }) => {
     (state) => state.campsites
   );
   const favorites = useSelector((state) => state.favorites);
+
   const renderFavoriteItem = ({ item: campsite }) => {
-    <ListItem
-      onPress={() =>
-        navigation.navigate("Directory", {
-          screen: "CampsiteInfo",
-          params: { campsite },
-        })
-      }
-    >
-      <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
-      <ListItem.Content>
-        <ListItem.Title>{campsite.name}</ListItem.Title>
-        <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
-      </ListItem.Content>
-    </ListItem>;
+    return (
+      <ListItem
+        onPress={() =>
+          navigation.navigate("Directory", {
+            screen: "CampsiteInfo",
+            params: { campsite },
+          })
+        }
+      >
+        <Avatar rounded source={{ uri: baseUrl + campsite.image }} />
+        <ListItem.Content>
+          <ListItem.Title>{campsite.name}</ListItem.Title>
+          <ListItem.Subtitle>{campsite.description}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
   };
+
   if (isLoading) {
     return <Loading />;
   }
