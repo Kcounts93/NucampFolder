99f296d1f9f742014191ec57e784cec48db70634
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-03-23 20:17:30 -0700

    server host

--- 5-NodeJS-Express-MongoDB/node-examples/app.js
+++ 5-NodeJS-Express-MongoDB/node-examples/app.js
@@ -1,24 +1,24 @@
-// import  default from ".rectangle";
+import rect from ".rectangle";
 
-// function solveRect(l, w) {
-//   console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
+function solveRect(l, w) {
+  console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
 
-//   rect(l, w, (err, rectangle) => {
-//     if (err) {
-//       console.log("ERROR:", err.message);
-//     } else {
-//       console.log(
-//         `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
-//       );
-//       console.log(
-//         `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
-//       );
-//     }
-//   });
-//   console.log("This statement is logged after the call to rect()");
-// }
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR:", err.message);
+    } else {
+      console.log(
+        `Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`
+      );
+      console.log(
+        `Perimeter of rectangle with dimensions ${l}, ${w} is: ${rectangle.perimeter()}`
+      );
+    }
+  });
+  console.log("This statement is logged after the call to rect()");
+}
 
-// solveRect(2, 4);
-// solveRect(3, 5);
-// solveRect(0, 5);
-// solveRect(5, -3);
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(5, -3);
