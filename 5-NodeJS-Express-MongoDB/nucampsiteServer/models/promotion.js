503af95f2a3dd5387e25577c814d1e2badff3a31
Kayla <95451731+Kcounts93@users.noreply.github.com>
2023-04-01 14:51:56 -0700

    workshop

---
+++ 5-NodeJS-Express-MongoDB/nucampsiteServer/models/promotion.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Currency = mongoose.Types.Currency;
+const Schema = mongoose.Schema;
+
+const promotionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    cost: {
+      type: Currency,
+      required: true,
+      min: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Promotion = mongoose.model("Promotion", partnerSchem);
+
+module.exports = Promotion;
